aec9ea1ada4a998f28a157ff38fc92def12b2579
Amit Kandar <kandaramit2001@gmail.com>
2024-08-12 18:28:06 +0530

    add error middleware

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,36 @@
+const logger = require('../config/logger');
+const APIError = require('../utils/APIError');
+
+const errorHandler = function (err, req, res, next) {
+    let statusCode = 500;
+    let message = "Internal Server Error";
+    let errors = [];
+    let data = null;
+
+    if (err instanceof APIError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        errors = err.errors;
+        data = err.data;
+    } else {
+        errors = [err.message];
+    }
+
+    // Log the error details
+    logger.error('Error occurred: ' + message, {
+        statusCode: statusCode,
+        errors: errors,
+        data: data,
+        stack: err.stack // Include stack trace if available
+    });
+
+    res.status(statusCode).json({
+        statusCode: statusCode,
+        success: false,
+        message: message,
+        errors: errors,
+        data: data,
+    });
+};
+
+module.exports = errorHandler;
