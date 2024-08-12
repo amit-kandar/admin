d9332b0a55f671f53ad99b3e91f3f55e97acdf43
Amit Kandar <kandaramit2001@gmail.com>
2024-08-12 18:31:01 +0530

    configured index file of the server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-const express = require('express');
+require("dotenv").config();
+const app = require('./app');
+const cloudinary = require('cloudinary').v2;
+const logger = require('./config/logger');
+const { connectToDatabase, synchronizeDatabase } = require('./database/index');
 
-const app = express();
-const PORT = 7070;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+// cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || '',
+    api_key: process.env.CLOUDINARY_API_KEY || '',
+    api_secret: process.env.CLOUDINARY_API_SECRET || ''
+});
+
+async function startServer() {
+    try {
+        // connect to database
+        await connectToDatabase();
+
+        // sync models with database
+        await synchronizeDatabase();
+
+        app.on("error", err => {
+            logger.error("Error: ", err);
+        })
+
+        app.listen(PORT, () => {
+            logger.info(`http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        logger.error('Error starting the server:', error);
+    }
+}
+
+startServer()
