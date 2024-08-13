9a97d749911e4f261eab61f2bc7fb130146c6102
Amit Kandar <kandaramit2001@gmail.com>
2024-08-13 12:10:24 +0530

    Added hooks and improves the field

--- src/models/Login.model.js
+++ src/models/Login.model.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize) => {
     const Login = sequelize.define('Login', {
@@ -10,15 +11,42 @@ module.exports = (sequelize) => {
         },
         f_userName: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+                len: [4, 50]
+            }
         },
         f_pwd: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [6, 100]
+            }
         }
     }, {
         tableName: "t_login",
-        timestamps: true
+        timestamps: true,
+        hooks: {
+            beforeCreate: async (login) => {
+                if (login.f_pwd) {
+                    login.f_pwd = await bcrypt.hash(login.f_pwd, 10);
+                }
+
+            },
+            beforeUpdate: async (login) => {
+                if (login.changed('f_pwd')) {
+                    login.f_pwd = await bcrypt.hash(login.f_pwd, 10);
+                }
+            }
+        }
     });
+
+    Login.prototype.comparePassword = async function (password) {
+        return bcrypt.compare(password, this.f_pwd)
+    }
+
     return Login;
 };
