55342e13147da4cecc65ec4beccb9a0dec9a7a76
Amit Kandar <kandaramit2001@gmail.com>
2024-08-14 12:48:47 +0530

    add three more route

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { upload } = require('../middlewares/multer.middleware');
-const { createEmployee } = require('../controllers/employee.controller');
+const { createEmployee, editEmployee, deleteEmployee, getAllEmployee } = require('../controllers/employee.controller');
 const router = express.Router();
 
 router.post('/', upload.single('f_image'), createEmployee);
+router.get('/', getAllEmployee)
+router.put('/:f_id', upload.single('f_image'), editEmployee);
+router.delete('/:f_id', deleteEmployee);
 
 module.exports = router;
